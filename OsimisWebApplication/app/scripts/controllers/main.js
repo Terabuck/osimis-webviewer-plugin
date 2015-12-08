3ae7f54e5edfb09b7697694e142653e61e2f1f9c
Thibault Piront <gse.nuks@gmail.com>
2015-12-08 19:51:18 +0100

    fix: remove bugged toolbar

--- OsimisWebApplication/app/scripts/controllers/main.js
+++ OsimisWebApplication/app/scripts/controllers/main.js
@@ -13,7 +13,6 @@ angular.module('osimiswebviewerApp')
 
     this.serieId = '5a45bfac-36d99323-bea29308-f7082c12-ee76150b';
     this.instanceIndex = 0;
-    this.toolbar = {};
 
     // Model: list series
     orthanc
