8063cd866876339db07a48a32ebdc209a25a57a0
Thibault Piront <gse.nuks@gmail.com>
2015-12-08 16:31:34 +0100

    feat: enhance overlay design

--- OsimisWebApplication/app/scripts/directives/wv-overlay.js
+++ OsimisWebApplication/app/scripts/directives/wv-overlay.js
@@ -10,26 +10,25 @@ angular.module('osimiswebviewerApp')
 .directive('wvOverlay', [function() {
   return {
     scope: false,
-    priority: 98,
     template: '<div class="wv-overlay">\
-                  <div class="wv-overlay-topleft">\
-                      PatientName: {{$instance.PatientName}}<br/>\
-                      PatientId: {{$instance.PatientID}}\
-                  </div>\
-                  <div class="wv-overlay-topright">\
-                      StudyDescription: {{$instance.StudyDescription}}<br/>\
-                      StudyDate: {{$instance.StudyDate}}\
-                  </div>\
-                  <div class="wv-overlay-bottomright">\
-                      zoom: {{$viewport.scale|number:2}}<br/>\
-                      ww/wc: {{$viewport.voi.windowWidth|number:0}}/{{$viewport.voi.windowCenter|number:0}}\
-                  </div>\
-                  <div class="wv-overlay-bottomleft">\
-                      {{$instance.InstanceNumber}}<span ng-show="$serie">/{{$serie.InstanceCount}}</span><br/>\
-                  </div>\
+                <div class="wv-overlay-topleft">\
+                    PatientName: {{$instance.PatientName}}<br/>\
+                    PatientId: {{$instance.PatientID}}\
+                </div>\
+                <div class="wv-overlay-topright">\
+                    StudyDescription: {{$instance.StudyDescription}}<br/>\
+                    StudyDate: {{$instance.StudyDate}}\
+                </div>\
+                <div class="wv-overlay-bottomright">\
+                    zoom: {{$viewport.scale|number:2}}<br/>\
+                    ww/wc: {{$viewport.voi.windowWidth|number:0}}/{{$viewport.voi.windowCenter|number:0}}\
+                </div>\
+                <div class="wv-overlay-bottomleft">\
+                    {{$instance.InstanceNumber}}<span ng-show="$serie">/{{$serie.InstanceCount}}</span><br/>\
+                </div>\
               </div>',
     restrict: 'E',
-    link: function postLink(scope, element, attrs) {
+    link: function postLink(scope, element, attrs, ctrl, transclude) {
       scope.$on('instance-data', function(evt, tags) {
         scope.$instance = tags;
       });
