c42e77472fee3c00156bfb710511b8b4de4cb895
Thibault Piront <gse.nuks@gmail.com>
2015-12-10 15:52:28 +0100

    feat: design scroll marker in overlay & remove overlay informations

--- OsimisWebApplication/app/scripts/directives/wv-overlay.js
+++ OsimisWebApplication/app/scripts/directives/wv-overlay.js
@@ -12,13 +12,14 @@ angular.module('osimiswebviewerApp')
     scope: false,
     template: '<div class="wv-overlay">\
                 <div class="wv-overlay-topleft">\
-                    PatientName: {{$instance.PatientName}}<br/>\
-                    PatientId: {{$instance.PatientID}}\
+                    {{$instance.PatientName}}<br/>\
+                    {{$instance.PatientID}}\
                 </div>\
                 <div class="wv-overlay-topright">\
-                    StudyDescription: {{$instance.StudyDescription}}<br/>\
-                    StudyDate: {{$instance.StudyDate}}\
+                    {{$instance.StudyDescription}}<br/>\
+                    {{$instance.StudyDate}}\
                 </div>\
+                <!--\
                 <div class="wv-overlay-bottomright">\
                     zoom: {{$viewport.scale|number:2}}<br/>\
                     ww/wc: {{$viewport.voi.windowWidth|number:0}}/{{$viewport.voi.windowCenter|number:0}}\
@@ -26,15 +27,27 @@ angular.module('osimiswebviewerApp')
                 <div class="wv-overlay-bottomleft">\
                     {{$instance.InstanceNumber}}<span ng-show="$serie">/{{$serie.InstanceCount}}</span><br/>\
                 </div>\
+                -->\
+                <<div style="position: absolute; bottom:0; left:0; height: 2px; background-color: red;" ng-style="{\
+                  width: nbWidth\
+                }">\
+                  <div style="position: absolute; left: 0px; top:calc(-1em - 5px); height: 1em; color: red; font-size: 0.8em;">\
+                    {{$instance.InstanceNumber}}<span ng-show="$serie">/{{$serie.InstanceCount}}</span><br/>\
+                  </div>\
+                </div>\
               </div>',
     restrict: 'E',
     link: function postLink(scope, element, attrs, ctrl, transclude) {
       scope.$on('instance-data', function(evt, tags) {
         scope.$instance = tags;
+        if (scope.$serie && scope.$serie.InstanceCount && scope.$instance && scope.$instance.InstanceNumber)
+          scope.nbWidth = 100/scope.$serie.InstanceCount * scope.$instance.InstanceNumber + '%';
       });
       scope.$on('serie-data', function(evt, tags, instanceCount) {
         scope.$serie = tags;
         scope.$serie.InstanceCount = instanceCount;
+        if (scope.$serie && scope.$serie.InstanceCount && scope.$instance && scope.$instance.InstanceNumber)
+          scope.nbWidth = 100/scope.$serie.InstanceCount * scope.$instance.InstanceNumber + '%';
       });
       scope.$on('viewport-data', function(evt, viewport) {
         scope.$viewport = viewport;
