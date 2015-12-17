50477f7ce465f978ac8affdb29d7b11a4be6c15a
Thibault Piront <gse.nuks@gmail.com>
2015-12-17 10:10:42 +0100

    refactor: make generic splitpane instead of multi-viewport

---
+++ OsimisWebApplication/app/scripts/directives/wv-splitpane.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name osimiswebviewerApp.directive:wvSplitpane
+ * @description
+ * # wvSplitpane
+ */
+angular.module('osimiswebviewerApp')
+  .directive('wvSplitpane', function () {
+    return {
+      scope: {
+        wvLayout: '=?'
+      },
+      templateUrl: 'scripts/directives/wv-splitpane.tpl.html',
+      restrict: 'E',
+      transclude: true,
+      link: function postLink(scope, element, attrs) {
+        if (scope.wvLayout == undefined) {
+          scope.wvLayout = {
+            x: 1,
+            y: 1
+          }
+        }
+        scope.x = [scope.wvLayout.x];
+        scope.y = [scope.wvLayout.y];
+
+        scope.rowHeight = 100 / scope.wvLayout.y + '%';
+        scope.rowWidth = 100 / scope.wvLayout.x + '%';
+        // scope.cellClasses = ['col-sm-' + 12/scope.wvLayout.x];
+        scope.$watch('wvLayout', function(wvLayout, old) {
+          scope.x = [wvLayout.x];
+          scope.y = [wvLayout.y];
+          scope.rowHeight = 100 / scope.wvLayout.y + '%';
+          scope.rowWidth = 100 / scope.wvLayout.x + '%';
+
+          $(window).resize();
+        }, true);
+      }
+    };
+  });
