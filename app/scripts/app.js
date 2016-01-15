62b41bce86244db24b32de0d878f586c88b8724a
Thibault Piront <gse.nuks@gmail.com>
2016-01-15 18:24:54 +0100

    chore: add package versions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,37 +1,40 @@
-'use strict';
+(function() {
+  'use strict';
 
-// @todo define angular var
+  var version = '0.1.2';
 
-/**
- * @ngdoc overview
- * @name osimiswebviewerApp
- * @description
- * # osimiswebviewerApp
- *
- * Main module of the application.
- */
-angular
-.module('webviewer', ['ngResource', 'ngSanitize', 'mgcrea.ngStrap', 'ngRangeFilter'])
-.provider('wvConfig', function() {
-  var defaultPath = _getScriptServerURL();
-  var _config = {
-    orthancApiURL: defaultPath,
-    webviewerApiURL: defaultPath + '/web-viewer',
-    defaultCompression: 'jpeg95'
-  };
+  /**
+   * @ngdoc overview
+   * @name osimiswebviewerApp
+   * @description
+   * # osimiswebviewerApp
+   *
+   * Main module of the application.
+   */
+  angular
+  .module('webviewer', ['ngResource', 'ngSanitize', 'mgcrea.ngStrap', 'ngRangeFilter'])
+  .provider('wvConfig', function() {
+    var defaultPath = _getScriptServerURL();
+    var _config = {
+      version: version,
+      orthancApiURL: defaultPath,
+      webviewerApiURL: defaultPath + '/web-viewer',
+      defaultCompression: 'jpeg95'
+    };
 
-  this.setApiURL = function(url) {
-    if (url.substr(-1) === '/') {
-      url = url.substr(0, url.length - 1);;
-    }
+    this.setApiURL = function(url) {
+      if (url.substr(-1) === '/') {
+        url = url.substr(0, url.length - 1);;
+      }
 
-    _config.orthancApiURL = url;
-    _config.webviewerApiURL = url + '/web-viewer';
-  };
+      _config.orthancApiURL = url;
+      _config.webviewerApiURL = url + '/web-viewer';
+    };
 
-  this.$get = function() {
-    return _config;
-  };
+    this.$get = function() {
+      return _config;
+    };
+  });
 
   function _getScriptServerURL() { // based on https://github.com/QueueHammer/codecraftsman.js/blob/master/codecraftsman.js
     var scriptPath = '';
@@ -51,4 +54,4 @@ angular
       return pathParts[1];
     }
   }
-});
+})();
