6aec3686397e12bde06c8d4a32e3b5672b6ac88d
Thibault Piront <gse.nuks@gmail.com>
2016-01-15 18:47:06 +0100

    chore: remove cdnify

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,6 @@ module.exports = function (grunt) {
   require('jit-grunt')(grunt, {
     useminPrepare: 'grunt-usemin',
     ngtemplates: 'grunt-angular-templates',
-    cdnify: 'grunt-google-cdn'
   });
 
   // Configurable paths for the application
@@ -274,13 +273,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
@@ -350,7 +342,6 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'usemin',
