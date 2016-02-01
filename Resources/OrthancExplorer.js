390bca8b89ecb459f3e062ee9197c01ecc7580db
Thibault Piront <gse.nuks@gmail.com>
2016-02-01 16:55:17 +0100

    feat: add study button

--- Resources/OrthancExplorer.js
+++ Resources/OrthancExplorer.js
@@ -6,13 +6,32 @@ $('#series').live('pagebeforecreate', function() {
     .attr('href', '#')
     .attr('data-icon', 'search')
     .attr('data-theme', 'e')
-    .text('Orthanc Web Viewer');
+    .text('Osimis Web Viewer');
 
   b.insertBefore($('#series-delete').parent().parent());
   b.click(function() {
     if ($.mobile.pageData) {
       var series = $.mobile.pageData.uuid;
-      window.open('../web-viewer/app/index.html?series=' + series);
+      window.open('../web-viewer/app/orthanc.html?series=' + series);
+    }
+  });
+});
+
+$('#study').live('pagebeforecreate', function() {
+  //$('#series-preview').parent().remove();
+
+  var b = $('<a>')
+    .attr('data-role', 'button')
+    .attr('href', '#')
+    .attr('data-icon', 'search')
+    .attr('data-theme', 'e')
+    .text('Osimis Web Viewer');
+
+  b.insertBefore($('#study-delete').parent().parent());
+  b.click(function() {
+    if ($.mobile.pageData) {
+      var study = $.mobile.pageData.uuid;
+      window.open('../web-viewer/app/orthanc.html?study=' + study);
     }
   });
 });
