d9d17d2c30ef8d0a045051a1d27b99da4eee3003
Thibault Piront <gse.nuks@gmail.com>
2016-02-17 16:17:13 +0100

    chore(tests): reduce karma verbosing

--- karma.conf.js
+++ karma.conf.js
@@ -26,12 +26,12 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress', 'coverage'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'coverage'],
-
-        coverageReporter: {
-            dir: gulpConfig.karma.coverage.dir,
-            reporters: gulpConfig.karma.coverage.reporters
-        },
+        reporters: ['mocha'/*, 'coverage'*/],
+        
+        // coverageReporter: {
+        //     dir: gulpConfig.karma.coverage.dir,
+        //     reporters: gulpConfig.karma.coverage.reporters
+        // },
 
         // web server port
         port: 9876,
@@ -42,7 +42,7 @@ module.exports = function(config) {
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR ||
         // config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+        logLevel: config.LOG_WARN,
 
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
