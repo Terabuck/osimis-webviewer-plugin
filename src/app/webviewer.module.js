2cc94d2b317b3e37294e9038bc8b81e62462f733
Thibault Piront <gse.nuks@gmail.com>
2016-02-29 15:44:02 +0100

    chore: set window globals as angular constants

--- src/app/webviewer.module.js
+++ src/app/webviewer.module.js
@@ -15,6 +15,8 @@
   .module('webviewer', ['ngResource', 'ngSanitize', 'mgcrea.ngStrap', 'ngRangeFilter', 'debounce'])
   .constant('$', window.$)
   .constant('_', window._)
+  .constant('pako', window.pako)
+  .constant('JpegImage', window.JpegImage)
   .constant('hamster', window.Hamster)
   .constant('cornerstone', window.cornerstone)
   .constant('cornerstoneTools', window.cornerstoneTools)
