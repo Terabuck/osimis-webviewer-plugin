22def61212cb09ac4c0b37361951235a3c29b357
Alain Mazy <am@osimis.io>
2016-02-29 18:32:20 +0100

    first ci.sh

---
+++ scripts/ci.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# this scripts generates the html/js/css output from the sources and pushes the changes into the build_dev/build_master branch
+# 
+# prerequisites:
+## install rvm for all users
+# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+# curl -L https://get.rvm.io | sudo bash -s stable
+# sudo usermod -G rvm jenkins
+# source ~/.rvm/scripts/rvm
+# rvm install 2.2.3
+# rvm use 2.2.3 --default
+# curl -sL https://deb.nodesource.com/setup_5.x | sudo bash -
+# sudo apt-get -y install nodejs
+# 
+# gem install compass
+# 
+# # install bower, grunt & grunt-cli
+# sudo npm install -g bower gulp nodemon
+
+set -e #to exit script at first command failure
+set -x #to debug the script
+
+startScriptDir=$(pwd)
+
+currentBranch=$(git rev-parse --abbrev-ref HEAD)
+buildBranch="build_$currentBranch"
+
+source /etc/profile.d/rvm.sh
+
+cd $startScriptDir/../
+npm install --unsafe-perm # note unsafe indeed
+gulp build
+npm cache clean
+
+cd $startScriptDir
