296365cc8746406472037be2a1c8092d0a10b5c0
Thibault Piront <gse.nuks@gmail.com>
2016-03-09 10:40:37 +0100

    chore(gulp): disable "gulp serve-build" notifiers

--- gulpfile.js
+++ gulpfile.js
@@ -742,14 +742,14 @@ function log(msg) {
  * Show OS level notification using node-notifier
  */
 function notify(options) {
-    var notifier = require('node-notifier');
-    var notifyOptions = {
-        sound: 'Bottle',
-        contentImage: path.join(__dirname, 'gulp.png'),
-        icon: path.join(__dirname, 'gulp.png')
-    };
-    _.assign(notifyOptions, options);
-    notifier.notify(notifyOptions);
+    // var notifier = require('node-notifier');
+    // var notifyOptions = {
+    //     sound: 'Bottle',
+    //     contentImage: path.join(__dirname, 'gulp.png'),
+    //     icon: path.join(__dirname, 'gulp.png')
+    // };
+    // _.assign(notifyOptions, options);
+    // notifier.notify(notifyOptions);
 }
 
 module.exports = gulp;
