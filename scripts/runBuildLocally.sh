8913a7af98e7fce2cb781ec6d1234f60548898d2
Thibault Piront <gse.nuks@gmail.com>
2016-04-14 12:44:49 +0200

    fix(ci): use dockerfile for javascript build dependencies

---
+++ scripts/runBuildLocally.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# @warning script should be launched from the osimis-webviewer root
+
+# npm cache clean # make sure install is fine
+npm install
+
+#first run the unit tests
+gulp test --novet # --novet disables jshint and jscs
+
+#run gulp build after the unit tests (unit tests seems to clean the gulp build output)
+gulp build
+
+# npm cache clean
