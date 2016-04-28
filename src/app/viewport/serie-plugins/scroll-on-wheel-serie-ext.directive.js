18f7632d18f60cf79364c25726e779cde54bc590
Thibault Piront <gse.nuks@gmail.com>
2016-04-28 14:23:52 +0200

    feat(serie.scroll): make compatible w/ mobile

--- src/app/viewport/serie-plugins/scroll-on-wheel-serie-ext.directive.js
+++ src/app/viewport/serie-plugins/scroll-on-wheel-serie-ext.directive.js
@@ -36,18 +36,23 @@
         var _wvSerieIdViewModels = [];
     	this.register = function(viewmodel) {
             _wvSerieIdViewModels.push(viewmodel);
+            registerMobileEvents(viewmodel);
     	};
     	this.unregister = function(viewmodel) {
             _.pull(_wvSerieIdViewModels, viewmodel);
+            unregisterMobileEvents(viewmodel);
     	};
         
+
+        /* desktop scrolling */
+
         hamster = hamster($element[0]);
 
         hamster.wheel(function(event, delta, deltaX, deltaY) {
             $scope.$apply(function() {
                 _wvSerieIdViewModels.forEach(function(viewmodel) {
                     var serie = viewmodel.getSerie();
-
+                    
                     if (!serie) {
                         return;
                     }
@@ -68,5 +73,66 @@
         $scope.$on('$destroy', function() {
             hamster.unwheel();
         });
+
+        /* mobile scrolling */
+
+        var hammertime = new Hammer($element[0]);
+        hammertime.get('pan').set({
+            direction: Hammer.DIRECTION_HORIZONTAL,
+            pointers: 1
+        });
+        
+        var _mobileEvtBySerieVM = {};
+        function registerMobileEvents(viewmodel) {
+            _mobileEvtBySerieVM[viewmodel] = onMobilePanning;
+            hammertime.on('pan', _mobileEvtBySerieVM[viewmodel]);
+
+            var _lastDistance = 0;
+            function onMobilePanning(evt) {
+                var serie = viewmodel.getSerie();
+                if (!serie) {
+                    return;
+                }
+
+                // sum distances of each evt call in a common var
+                // for some reasons distance = 1 seems to equals 20 pixels..
+                _lastDistance += evt.distance / 20;
+
+                // each time the distance attains the required width
+                // switch image & reset the lastDistance
+                var requiredDistance = ($element.width() / serie.imageCount);
+                if (_lastDistance >= requiredDistance) {
+                    $scope.$apply(function() {
+                        if (evt.direction === Hammer.DIRECTION_LEFT) {
+                            serie.goToPreviousImage();
+                        }
+                        else if (evt.direction === Hammer.DIRECTION_RIGHT) {
+                            serie.goToNextImage(false);
+                        }
+                    });
+                    
+                    // set lastDistance to the remaining distance
+                    _lastDistance = _lastDistance % requiredDistance;
+                }
+
+                // reset the distance when the gesture starts
+                if (evt.isFinal) {
+                    _lastDistance = 0;
+                }
+            }
+        };
+        function unregisterMobileEvents(viewmodel) {
+            hammertime.off('pan', _mobileEvtBySerieVM[viewmodel])
+        }
+        $scope.$on('$destroy', function() {
+            hammertime.destroy();
+        });
+
+
+        // listen to events...
+        //mc.on("panleft panright panup pandown tap press", function(ev) {
+        //    myElement.textContent = ev.type +" gesture detected.";
+        //});
+
     }
 })();
