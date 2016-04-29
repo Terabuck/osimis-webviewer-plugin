00e460f61a9c08fa325d58b481f829cfcf7601ec
Alain Mazy <am@osimis.io>
2016-04-29 13:58:32 +0200

    using version from version.txt instead of git tag

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,11 @@ FROM jodogne/orthanc-plugins
 RUN DEBIAN_FRONTEND=noninteractive apt-get update; apt-get -y install libgdcm2-dev libjpeg-dev git; rm -rf /var/lib/apt/lists/*
 
 ADD . /root/osimis-webviewer
-WORKDIR /root/osimis-webviewer
-RUN ls -al
-RUN git status
-#RUN git describe
 
-#RUN bash /root/osimis-webviewer/scripts/dockerBuildWebViewer.sh "default"
+RUN bash /root/osimis-webviewer/scripts/dockerBuildWebViewer.sh "default"
 
-#EXPOSE 4242
-#EXPOSE 8042
+EXPOSE 4242
+EXPOSE 8042
 
-#ENTRYPOINT [ "Orthanc" ]
-#CMD [ "/etc/orthanc/orthanc.json" ]
+ENTRYPOINT [ "Orthanc" ]
+CMD [ "/etc/orthanc/orthanc.json" ]
--- Resources/CMake/GetProductVersionFromGitTag.cmake
+++ Resources/CMake/GetProductVersionFromGitTag.cmake
@@ -2,19 +2,36 @@ if(NOT GIT_FOUND)
     find_package(Git QUIET)
 endif()
 
-execute_process(COMMAND
-    "${GIT_EXECUTABLE}"
-    describe --tags --long --dirty=-dirty
-    WORKING_DIRECTORY
-    "${CMAKE_SOURCE_DIR}"
-    RESULT_VARIABLE
-    res
-    OUTPUT_VARIABLE
-    gitRepoVersion
-    ERROR_QUIET
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-if(NOT res EQUAL 0)
-    message(FATAL_ERROR "could not describe git tag.  Make sure you have already tagged your repo with a command like 'git tag -a \"v0.1.0\"' ${gitRepoVersion}-${res}")
+if (false) #since we can't get gitTags on DockerHub, we'll use the version from version.txt
+    execute_process(COMMAND
+        "${GIT_EXECUTABLE}"
+        describe --tags --long --dirty=-dirty
+        WORKING_DIRECTORY
+        "${CMAKE_SOURCE_DIR}"
+        RESULT_VARIABLE
+        res
+        OUTPUT_VARIABLE
+        gitRepoVersion
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(NOT res EQUAL 0)
+        message(FATAL_ERROR "could not describe git tag.  Make sure you have already tagged your repo with a command like 'git tag -a \"v0.1.0\"' ${gitRepoVersion}-${res}")
+    endif()
+else()
+    file (STRINGS "version.txt" version)
+    execute_process(COMMAND
+        "${GIT_EXECUTABLE}"
+        rev-parse --short HEAD
+        WORKING_DIRECTORY
+        "${CMAKE_SOURCE_DIR}"
+        RESULT_VARIABLE
+        res
+        OUTPUT_VARIABLE
+        gitRepoCommitSha1
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    set(gitRepoVersion "${version}-0-${gitRepoCommitSha1}")
 endif()
 
 message("CMAKE_SOURCE_DIR = ${CMAKE_SOURCE_DIR}")
