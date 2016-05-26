62dcce9bbf483e25382cd34550c3225ba823771f
Thibault Piront <gse.nuks@gmail.com>
2016-05-26 20:42:20 +0200

    fix ci

--- src/app/image/image-parser.worker/main.js
+++ src/app/image/image-parser.worker/main.js
@@ -33,7 +33,7 @@ importScripts('/config.js');
 var OrthancApiURL = window.orthancUrl;
 if (OrthancApiURL.indexOf('://') === -1) {
     // Remove bad url/cors issue induced by blob protocol.
-    OrthancApiUrl = location.origin + '/' + OrthancApiURL;
+    OrthancApiURL = location.origin + '/' + OrthancApiURL;
 }
 // @todo out..
 var Qualities = {
