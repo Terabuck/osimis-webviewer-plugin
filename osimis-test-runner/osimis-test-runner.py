53aea63ab3bdbdb1649e54d8325fd7c4b6026ea4
Thibault Piront <gse.nuks@gmail.com>
2016-06-29 14:51:48 +0200

    feat(tests): exit the test runner with error code on failure (for CI)

--- osimis-test-runner/osimis-test-runner.py
+++ osimis-test-runner/osimis-test-runner.py
@@ -12,19 +12,21 @@ Install:
 Usage:
 - . env/bin/activate
 - python osimis-test-runner.py [--auto-watch|-w] [--orthanc-path=|-p=]
+
+Example:
+- python osimis-test-runner.py -w -p ../../osimis-webviewer-plugin/BuildDev/
 """
 
 from orthancServer import OrthancServer, OrthancServerVersion
 from orthancRestApi import OrthancClient
 from helpers import LogHelpers
+from termcolor import colored
 import logging
-import os
-import shlex, subprocess
-import sys, getopt
+import os, sys, getopt, shlex, subprocess, json
 
 # Parse command line attributes
 argv = sys.argv[1:]
-orthancFolder = './orthanc'
+orthancFolder = os.path.realpath('./orthanc')
 singleRun = True
 try:
 	opts, args = getopt.getopt(argv, "hwp:", ["auto-watch", "orthanc-path="])
@@ -38,11 +40,11 @@ for opt, arg in opts:
 	elif opt in ("-w", "--auto-watch"):
 		singleRun = False
 	elif opt in ("-p", "--orthanc-path"):
-		orthancFolder = arg
+		orthancFolder = os.path.realpath(arg)
 
 dicomSamplesFolder = 'dicom-samples/'
 orthancPort1 = 7414
-orthancPort2 = 7415 # http port
+orthancPort2 = 8042 # http port
 
 # Download Orthanc server (if not in path)
 OrthancServer.loadExecutable(orthancFolder, OrthancServerVersion.NIGHTLY)
@@ -52,6 +54,7 @@ OrthancServer.executableFolder = orthancFolder
 server = OrthancServer('Files', 'Files', orthancPort1, orthancPort2)
 server.config['HttpCompressionEnabled'] = False
 server.setStdoutCallback(lambda msg: print('[ORT] ' + msg))
+server.addPlugin('OsimisWebViewer')
 server.launch()
 
 # Init Orthanc client
@@ -59,19 +62,35 @@ client = OrthancClient('http://127.0.0.1:' + str(orthancPort2))
 
 # Uploading dicom files
 # @todo use instances = client.uploadFolder(dicomSamplesFolder) once fixed
-instancesId = []
+instancesIds = []
 for path in os.listdir(dicomSamplesFolder):
     imagePath = os.path.join(dicomSamplesFolder, path)
     print(imagePath)
     if os.path.isfile(imagePath) and '/.' not in imagePath:
-        instancesId.append(client.uploadDicomFile(imagePath))
+        instancesIds.append(client.uploadDicomFile(imagePath))
+
+# List instances for testing purpose
+print(colored('Instances:', 'blue'));
+print(colored(json.dumps(instancesIds, sort_keys=True, indent=4), 'blue'));
 
 # Launch karma
 karma = subprocess.Popen(
 	shlex.split('./node_modules/karma/bin/karma start karma.conf.js' + (' --single-run' if singleRun else '')),
 	cwd = '..' # set cwd path to ../ so bower.json can be found by karma.conf.js
 )
-karma.wait()
 
 # Stop Orthanc once karma has fininshed
+karmaReturnCode = None
+try:
+	karmaReturnCode = karma.wait(timeout = 60*5 if singleRun else None) # kill after 5 min
+except:
+	print(colored('error: karma timeout expired', 'red'))
+	server.stop()
+	karma.kill()
+	sys.exit(1)
+
 server.stop()
+
+# Exit the script with error if karma failed (for CI purpose)
+if karmaReturnCode != 0:
+	sys.exit(1)
