055d42a455e0cba4b1fb05be0eddeeff9cf66cbf
Thibault Piront <gse.nuks@gmail.com>
2016-07-10 17:51:27 +0200

    fix(series): case sensitivity fs issue: rename series directory to Series

---
+++ Plugin/Series/SeriesFactory.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <json/value.h>
+#include "../../Orthanc/Core/DicomFormat/DicomMap.h"
+#include "../AvailableQuality/IAvailableQualityPolicy.h"
+#include "Series.h"
+
+class SeriesFactory : public boost::noncopyable {
+public:
+  SeriesFactory(std::auto_ptr<IAvailableQualityPolicy> availableQualityPolicy); // takes ownership
+
+  std::auto_ptr<Series> CreateSeries(const std::string& seriesId, const Json::Value& slicesShort,
+      const Orthanc::DicomMap& metaInfoTags, const Json::Value& otherTags);
+
+private:
+  const std::auto_ptr<IAvailableQualityPolicy> _availableQualityPolicy;
+};
---
+++ Plugin/Series/SeriesRepository.cpp
@@ -0,0 +1,59 @@
+#include "SeriesRepository.h"
+
+#include <string>
+#include <json/value.h>
+#include <boost/scope_exit.hpp>
+#include <boost/pointer_cast.hpp>
+#include "../../Orthanc/Core/OrthancException.h"
+#include "../../Orthanc/Core/DicomFormat/DicomMap.h"
+
+#include "../OrthancContextManager.h"
+#include "../Image/AvailableQuality/OnTheFlyDownloadAvailableQualityPolicy.h"
+#include "ViewerToolbox.h"
+
+SeriesRepository::SeriesRepository(DicomRepository* dicomRepository)
+  : _dicomRepository(dicomRepository), _seriesFactory(std::auto_ptr<IAvailableQualityPolicy>(new OnTheFlyDownloadAvailableQualityPolicy))
+{
+}
+
+std::auto_ptr<Series> SeriesRepository::GetSeries(const std::string& seriesId) {
+  OrthancPluginContext* context = OrthancContextManager::Get();
+  
+  // Retrieve series' slices (instances & frames)
+  Json::Value orderedSlices;
+  if (!OrthancPlugins::GetJsonFromOrthanc(orderedSlices, context, "/series/" + seriesId + "/ordered-slices"))
+  {
+    throw Orthanc::OrthancException(static_cast<Orthanc::ErrorCode>(OrthancPluginErrorCode_InexistentItem));
+  }
+
+  // Retrieve middle instance id
+  Json::Value slicesShort = orderedSlices["SlicesShort"];
+  int instanceCount = slicesShort.size();
+  std::string instanceId = slicesShort[instanceCount / 2][0].asString();
+
+  // Get instance's dicom file
+  OrthancPluginMemoryBuffer dicom; // no need to free - memory managed by dicomRepository
+  _dicomRepository->getDicomFile(instanceId, dicom);
+
+  // Clean dicom file (at scope end)
+  BOOST_SCOPE_EXIT(_dicomRepository, &instanceId) {
+    _dicomRepository->decrefDicomFile(instanceId);
+  } BOOST_SCOPE_EXIT_END;
+
+  // Get instance's tags (the DICOM meta-informations)
+  Orthanc::DicomMap tags1;
+  if (!Orthanc::DicomMap::ParseDicomMetaInformation(tags1, reinterpret_cast<const char*>(dicom.data), dicom.size))
+  {
+    throw Orthanc::OrthancException(static_cast<Orthanc::ErrorCode>(OrthancPluginErrorCode_CorruptedFile));
+  }
+
+  // Get instance's tags (the other tags)
+  Json::Value tags2;
+  if (!OrthancPlugins::GetJsonFromOrthanc(tags2, context, "/instances/" + instanceId + "/simplified-tags"))
+  {
+    throw Orthanc::OrthancException(static_cast<Orthanc::ErrorCode>(OrthancPluginErrorCode_InexistentItem));
+  }
+
+  // Create a series based on tags and ordered instances
+  return std::auto_ptr<Series>(_seriesFactory.CreateSeries(seriesId, slicesShort, tags1, tags2));
+}
