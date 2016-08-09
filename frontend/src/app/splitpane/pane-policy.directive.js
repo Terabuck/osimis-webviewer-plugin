18f80959f9826da6ae15085978b80fe0be2c3c4b
Thibault Piront <gse.nuks@gmail.com>
2016-08-09 22:54:59 +0200

    refactor(splitpane): ! adapt to new conventions & add documentation
    also, $scope.$evalAsync is used to trigger $(window).resize()
    instead of $timeout. It may affect usage.

--- frontend/src/app/splitpane/pane-policy.directive.js
+++ frontend/src/app/splitpane/pane-policy.directive.js
@@ -1,3 +1,73 @@
+/**
+ * @ngdoc
+ *
+ * @name wvPanePolicy
+ *
+ * @description
+ * The `wvPanePolicy` directive configure the content of the `wvSplitpane` directive.
+ * It provides standard access to the user scope. Pane properties can be retrieved
+ * via additional attributes.
+ *
+ * @require wvSplitpane
+ *
+ * @scope
+ * 
+ * @restrict E
+ *
+ * @param {integer} wvPaneIndex (optional, readonly) Provide the index of the pane
+ *
+ * @param {object} wvPanePosition (optional, readonly) Provide the position of the pane within the
+ *   splitpane
+ *   * `x` The column index - starts from 0
+ *   * `y` The row index - starts from 0
+ *
+ * @param {function} wvOnPaneAdded (optional, callback) Called when the pane is added
+ *   Useful to provide external configuration. For instance, save and retrieve the pane
+ *   state on a database.
+ *   Callback attributes arguments:
+ *   `$index` - provide wvPaneIndex like data
+ *   `$position` - provide wvPanePosition like parameter
+ *
+ * @param {function} wvOnPaneRemoved (optional, callback) Called when the pane is removed
+ *   Useful to provide external cleanup.
+ *   Callback attributes arguments:
+ *   `$index` - provide `wvPaneIndex` like data
+ *   `$position` - provide `wvPanePosition` like parameter
+ *
+ * @example
+ * The following example display a 2x2 layout of viewports. The series ids are stored on an array.
+ * The pane is bound to a context external of the splitpane one, via vm.viewports array,
+ * configure & cleanup methods. The external context is accessed via the pane-policy using
+ * the `wvPaneIndex` attribute via vm.viewports[$index].
+ * 
+ * ```js
+ * // Configure viewports by index
+ * var defaultSeriesId = 'your-series';
+ * vm.viewports = [];
+ * vm.configureViewport = function(index) {
+ *     vm.viewports[index] = {
+ *         seriesId: defaultSeriesId
+ *     };
+ * };
+ * vm.cleanupViewport = function(index) {
+ *     vm.viewports[index] = undefined; // don't use splice since it changes the indexes from the array
+ * }; 
+ * ```
+ * 
+ * ```html
+ * <wv-splitpane wv-layout="{x: 2, y: 2}">
+ *     <wv-pane-policy wv-pane-index="$index" wv-pane-position="$position"
+ *         wv-on-pane-added="vm.configureViewport($index)"
+ *         wv-on-pane-removed="vm.cleanupViewport($index)"
+ *         wv-size-tag
+ *     >
+ *         <wv-viewport vp:series-id="vm.viewports[$index].seriesId"
+ *                      wv-size="{width: '[wv-size-tag]', height: '[wv-size-tag]'}"
+ *         ></wv-viewport>
+ *     </wv-pane-policy>
+ * </wv-splitpane>
+ * ```
+ **/
 (function() {
     'use strict';
 
@@ -47,21 +117,18 @@
             var contextScope = scope.$parent.$parent;
             
             // Configure paneIndex and panePosition
-            vm.paneIndex = contextScope.$index;
-            vm.panePosition = vm.panePosition || {
-                x: contextScope.$x,
-                y: contextScope.$y
-            };
 
             // Set wvPanePolicy directive attributes based on $index, $x and $y from wvSplitpane context
             var $x = contextScope.$x;
             var $y = contextScope.$y;
-            vm.panePosition.x = $x;
-            vm.panePosition.x = $x;
-
             var layout = contextScope.wvLayout;
-            vm.paneIndex = ($y*layout.x) + ($x);
             
+            vm.paneIndex = ($y*contextScope.vm.layout.x) + ($x);
+            vm.panePosition = {
+                x: $x,
+                y: $y
+            };
+
             // Call onAdded and onRemoved callbacks
             if (vm.onAdded) {
                 vm.onAdded({
--- frontend/src/app/splitpane/splitpane.directive.js
+++ frontend/src/app/splitpane/splitpane.directive.js
@@ -1,46 +1,85 @@
-'use strict';
-
 /**
- * @ngdoc directive
- * @name webviewer.directive:wvSplitpane
+ * @ngdoc
+ *
+ * @name wvSplitpane
+ *
  * @description
- * # wvSplitpane
- */
-angular.module('webviewer')
-.directive('wvSplitpane', function ($, $timeout) {
-    return {
-        scope: {
-            wvLayout: '=?'
-        },
-        templateUrl: 'app/splitpane/splitpane.directive.html',
-        restrict: 'E',
-        transclude: {
-            panePolicy: 'wvPanePolicy'
-        },
-        controller: function() {}, // Must be present for 'require' to work within wvPanePolicy
-        link: function postLink(scope, element, attrs) {
-          /* jshint -W116 */
-              scope.wvLayout = scope.wvLayout || {
-                x: 1,
-                y: 1
-              };
-          /* jshint +W116 */
+ * The `wvSplitpane` directive provides multiple pane organized on a grid.
+ * The pane quantity depends on its configuration. Panes must be configured
+ * using the `wvPanePolicy` directive. See the example.
+ * It triggers $(window).resize() on layout change.
+ * For additional configuration option, see the specific `wvPanePolicy` source code.
+ *
+ * @scope
+ * 
+ * @restrict E
+ *
+ * @param {object} wvLayout (optional) Define the quantity of row and column
+ *   * `x` The number of row
+ *   * `y` The number of column
+ *   Default: {x: 1, y: 1}
+ *
+ * @example
+ * ```html
+ * <wv-splitpane wv-layout="{x: 2, y: 2}">
+ *     <wv-pane-policy>
+ *         This will be repeated 4 times.
+ *     </wv-pane-policy>
+ * </wv-splitpane>
+ * ```
+ **/
+ (function() {
+    'use strict';
 
-          scope.$watch('wvLayout', _updateLayout, true);
+    angular
+        .module('webviewer')
+        .directive('wvSplitpane', wvSplitpane);
 
-          function _updateLayout(layout, old) {
-            if (!layout) return;
+    /* @ngInject */
+    function wvSplitpane() {
+        var directive = {
+            bindToController: true,
+            controller: Controller,
+            controllerAs: 'vm',
+            link: link,
+            restrict: 'E',
+            scope: {
+                layout: '=?wvLayout'
+            },
+            templateUrl: 'app/splitpane/splitpane.directive.html',
+            transclude: {
+                panePolicy: 'wvPanePolicy'
+            }
+        };
+        return directive;
 
-            scope.x = [layout.x];
-            scope.y = [layout.y];
-            scope.rowHeight = 100 / layout.y + '%';
-            scope.rowWidth = 100 / layout.x + '%';
+        function link(scope, element, attrs) {
 
-            // @note trigger this after reflow
-            $timeout(function() {
+        }
+    }
+
+    /* @ngInject */
+    function Controller($scope) {
+        var vm = this;
+
+        /* jshint -W116 */
+        vm.layout = vm.layout || {
+            x: 1,
+            y: 1
+        };
+        /* jshint +W116 */
+
+        // Trigger $(window).resize() on layout change & set row & columns width
+        $scope.$watch('vm.layout', _updateLayout, true);
+        function _updateLayout(newLayout) {
+            if (!newLayout) return;
+
+            vm.rowHeight = 100 / newLayout.y + '%';
+            vm.rowWidth = 100 / newLayout.x + '%';
+
+            $scope.$evalAsync(function() {
                 $(window).resize();
             });
-          }
         }
-  };
-  });
+    }
+})();
