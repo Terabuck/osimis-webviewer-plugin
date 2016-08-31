5a5deab64bbdc39e2bbf5966c2a38604a815ecb2
Alain Mazy <am@osimis.io>
2016-08-31 16:09:49 +0200

    referencing 3rd party downloads on orthanc.osimis.io

--- backend/Resources/CMake/GdcmConfiguration.cmake
+++ backend/Resources/CMake/GdcmConfiguration.cmake
@@ -44,7 +44,7 @@ if (STATIC_BUILD OR NOT USE_SYSTEM_GDCM)
 
   include(ExternalProject)
   externalproject_add(GDCM
-    URL "http://www.montefiore.ulg.ac.be/~jodogne/Orthanc/ThirdPartyDownloads/gdcm-2.6.0.tar.gz"
+    URL "http://orthanc.osimis.io/ThirdPartyDownloads/gdcm-2.6.0.tar.gz"
     URL_MD5 "978afe57af448b1c97c9f116790aca9c"
     CMAKE_ARGS -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE} ${Flags}
     #-DLIBRARY_OUTPUT_PATH=${CMAKE_CURRENT_BINARY_DIR}
