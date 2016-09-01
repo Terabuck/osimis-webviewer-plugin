c737677c89cde8a0dd961521c784810ee3ef32c7
Thibault Piront <gse.nuks@gmail.com>
2016-09-01 12:21:57 +0200

    fix(test-helpers): update test helpers to asynchronous mode

--- frontend/src/test-helpers/osimis-helpers.js
+++ frontend/src/test-helpers/osimis-helpers.js
@@ -5,117 +5,71 @@
         inject: inject,
         beforeEach: beforeEach,
         afterEach: afterEach,
-        sync: sync,
-        flush: flush,
-        digest: digest,
         directive: directive,
         controller: controller
     });
 
     function inject() {
-        bard.inject('$compile', '$q', '$timeout', '$httpBackend', '$rootScope', '$timeout', 'cornerstone');
+        bard.inject('$compile', '$q', '$timeout', '$rootScope', '$timeout', 'cornerstone');
     }
 
     function afterEach() {
         window.afterEach(function() {
+            $scope.$destroy();
             $rootScope.$destroy();
             $('body').children().remove();
 
             cornerstone.imageCache.purgeCache();
-            flush();
         });
     }
 
     function beforeEach() {
         window.beforeEach(function() {
-            bard.appModule('webviewer', 
+            bard.asyncModule('webviewer', 
                 function(wvConfigProvider) {
-                    wvConfigProvider.setApiURL('/');
+                    wvConfigProvider.setApiURL(window.orthancUrl || '/');
                 },
                 function($exceptionHandlerProvider) {
                     $exceptionHandlerProvider.mode('rethrow');
+                },
+                function ($compileProvider) {
+                    $compileProvider.debugInfoEnabled(true);
                 }
             );
 
             inject();
 
-            _.forEach(orthanc.raw, function(data, path) {
-                $httpBackend
-                    .when('GET', '/' + path)
-                    .respond(data);
-            });
-
             // for console live debugging purpose
             window.test = this.currentTest;
 
-            window.$scope = window.$rootScope;
-
-            window.requestAnimationFrame = function(cb) { 
-                $timeout(cb, 0, false);
-            };
-            window.cancelAnimationFrame = angular.noop;
+            window.$scope = window.$rootScope.$new();
         });
     }
 
-    function sync(promise) {
-        var result = null;
-        promise.then(function(result_) {
-            result = result_;
-        });
-        flush();
-        return result;
-    }
-
-    function flush() {
-        try {
-            $timeout.flush(); // flush pending promises (& apply)
-            
-            // recursive flush till 'No deffered tasks to be flushed' exception
-            flush();
-        }
-        catch(e) {
-            if (e.message === 'No deferred tasks to be flushed') {
-                // done
-            }
-            else {
-                // rethrows error occuring during $digest
-                throw e;
-            }
-        }
-        try {
-            $httpBackend.flush(); // flush pending requests
-            
-            // recursive flush till 'No deffered tasks to be flushed' exception
-            flush();
-        }
-        catch(e) {
-            if (e.message === 'No pending request to flush !') {
-                // ignore "no pending requests exceptions"
-                ;
-            }
-            else {
-                // rethrows error occuring during $digest
-                throw e;
-            }
-        }
-    }
-
-    function digest() {
-        $rootScope.$digest();
-        flush();
-    }
-
+    // see https://docs.angularjs.org/api/ng/function/angular.element for available methods
     function directive(html) {
         var domElement = $(html);
 
         // @note element must appended to body to retrieve its size
         $('body').append(domElement);
 
-        var element = $compile(domElement)($rootScope);
+        var element = $compile(domElement)(window.$scope);
 
-        digest();
+        window.$rootScope.$digest();
 
-        return element;
+        return $q(function(resolve, reject) {
+            // Force a setTimeout after the $digest (somehow due to AngularJS testing intricaties when bardjs
+            // is used in async mode, even if $digest should be synchronous anyway)
+            // Note $q don't trigger setTimeout like most promise systems do because it relies on $digest cycle instead.
+            // Therefore, we have to manually call the timeout.
+            setTimeout(function() {
+                // Add helpers
+                element.$controller = controller(element, domElement.prop('tagName').toLowerCase());
+                element.$scope = element.isolateScope() || element.scope();
+
+                resolve(element);
+            });
+        });
     }
 
     function controller(element, name) {
@@ -126,10 +80,10 @@
             .split('-')
             .map(function(token, index) {
                 if (index === 0) {
-                    return token
+                    return token.toLowerCase()
                 }
                 else {
-                    return token.charAt(0).toUpperCase() + token.slice(1);
+                    return token.charAt(0).toUpperCase() + token.slice(1).toLowerCase();
                 }
             })
             .join('');
