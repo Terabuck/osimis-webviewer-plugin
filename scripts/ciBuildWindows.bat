dc76ef0d4110ecc9a7b8dac9e4999ef572d9d2bd
Alain Mazy <am@osimis.io>
2016-09-06 21:55:27 +0200

    fix

--- scripts/ciBuildWindows.bat
+++ scripts/ciBuildWindows.bat
@@ -4,8 +4,10 @@ python -m venv env
 CALL env\Scripts\activate.bat
 
 pip install -e git+https://bitbucket.org/osimis/build-helpers.git@0.3.0#egg=buildHelpers
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-python buildWindowsOSX.py
+python buildWindowsOsx.py
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 CALL deactivate
 
