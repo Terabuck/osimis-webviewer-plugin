f1a8594d084ba2530b07788b0cf408ff3c61ec22
Thibault Piront <gse.nuks@gmail.com>
2016-09-26 13:48:58 +0200

    feat(ci): limit ci docker network to 4 ips

--- scripts/ciPrepareTests.sh
+++ scripts/ciPrepareTests.sh
@@ -18,5 +18,5 @@ docker rm -v $testedContainerId > /dev/null
 # Prepare unit tests
 echo "Prepare karma unit test environment.."
 echo "Create network ${TEST_NETWORK}"
-docker network create ${TEST_NETWORK} || true # see https://github.com/docker/compose/issues/3068
+docker network create ${TEST_NETWORK} --subnet=10.0.0.0/29 || true # manual network create, see https://github.com/docker/compose/issues/3068 (also, limit to 4 ips / class A)
 docker-compose -f $TEST_COMPOSE_FILE -p $TEST_COMPOSE_PROJECT create --build # do not use --force-recreate (invalidate images cache - not stated in doc)
