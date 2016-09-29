833f44d692e1ae434db304bc2db3594bbba2afcb
Thibault Piront <gse.nuks@gmail.com>
2016-09-29 14:35:54 +0200

    chore(build): use CXX11
    # Conflicts:
    #	backend/CMakeLists.txt

--- backend/WebViewerLibrary/WebViewerLibrary.cmake
+++ backend/WebViewerLibrary/WebViewerLibrary.cmake
@@ -48,6 +48,18 @@ set(USE_SYSTEM_JSONCPP ON CACHE BOOL "Use the system version of JsonCpp")
 set(USE_SYSTEM_SQLITE ON CACHE BOOL "Use the system version of SQLite")
 set(USE_SYSTEM_ORTHANC_SDK ON CACHE BOOL "Use the system version of the Orthanc plugin SDK")
 
+# Use c++11
+include(CheckCXXCompilerFlag)
+CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
+CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
+if(COMPILER_SUPPORTS_CXX11)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+elseif(COMPILER_SUPPORTS_CXX0X)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+else()
+  message(STATUS "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
+endif()
+
 # Build dependencies
 set(ORTHANC_ROOT ${ORTHANC_DIR}) # required by orthanc's cmake
 include(CheckIncludeFiles)
