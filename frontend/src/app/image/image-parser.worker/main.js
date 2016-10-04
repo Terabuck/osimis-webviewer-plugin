41bf95d03a2512e77097882d4509b799a6b2fc58
Thibault Piront <gse.nuks@gmail.com>
2016-10-04 20:18:59 +0200

    refactor(worker): remove useless logging
    Mocha doesn't show worker logs, so we added these logs.
    
    From now on, logs should be read from
    chrome when doing unit tests http://localhost:9876/debug.html
    Keeping these logs makes it to verbose to develop with the
    chrome console.

--- frontend/src/app/image/image-parser.worker/main.js
+++ frontend/src/app/image/image-parser.worker/main.js
@@ -19,72 +19,18 @@
 
 // @todo move jpgjs & pngjs out of bower_components
 
-// Polyfill ArrayBuffer#slice for IE10
-// see http://stackoverflow.com/questions/21440050/arraybuffer-prototype-slice-shim-for-ie
-if (!ArrayBuffer.prototype.slice) {
-    //Returns a new ArrayBuffer whose contents are a copy of this ArrayBuffer's
-    //bytes from `begin`, inclusive, up to `end`, exclusive
-    ArrayBuffer.prototype.slice = function (begin, end) {
-        //If `begin` is unspecified, Chrome assumes 0, so we do the same
-        if (begin === void 0) {
-            begin = 0;
-        }
+// Load libraries
 
-        //If `end` is unspecified, the new ArrayBuffer contains all
-        //bytes from `begin` to the end of this ArrayBuffer.
-        if (end === void 0) {
-            end = this.byteLength;
-        }
-
-        //Chrome converts the values to integers via flooring
-        begin = Math.floor(begin);
-        end = Math.floor(end);
-
-        //If either `begin` or `end` is negative, it refers to an
-        //index from the end of the array, as opposed to from the beginning.
-        if (begin < 0) {
-            begin += this.byteLength;
-        }
-        if (end < 0) {
-            end += this.byteLength;
-        }
-
-        //The range specified by the `begin` and `end` values is clamped to the 
-        //valid index range for the current array.
-        begin = Math.min(Math.max(0, begin), this.byteLength);
-        end = Math.min(Math.max(0, end), this.byteLength);
-
-        //If the computed length of the new ArrayBuffer would be negative, it 
-        //is clamped to zero.
-        if (end - begin <= 0) {
-            return new ArrayBuffer(0);
-        }
-
-        var result = new ArrayBuffer(end - begin);
-        var resultBytes = new Uint8Array(result);
-        var sourceBytes = new Uint8Array(this, begin, end - begin);
-
-        resultBytes.set(sourceBytes);
-
-        return result;
-    };
-}
+// Import ArrayBuffer polyfill
+/* @inline: */ importScripts('/app/image/image-parser.worker/array-buffer.polyfill.js');
 
 // Import user agent parser
 /* @inline: */ importScripts('/bower_components/ua-parser-js/dist/ua-parser.min.js');
 var uaParser = (new UAParser()).getResult();
 
-// Import bluebird promise polyfill (wrap in try catch for enhanced logging)
-try {
-/* @inline: */ importScripts('/bower_components/bluebird/js/browser/bluebird.js');
-}
-catch(e) {
-    console.error('WORKER ERROR');
-    console.error(e.message);
-    console.error(e.stack);
-    console.error(e.stacktrace);
-    throw new Error(JSON.stringify(e.stack));
-}
+// Import bluebird promise polyfill
+/* @inline: */ importScripts('/bower_components/bluebird/js/browser/bluebird.min.js');
+
 // Import osimis.HttpRequest
 /* @inline: */ importScripts('/app/http/http-request.class.js');
 
@@ -95,7 +41,7 @@ catch(e) {
 /* @inline: */ importScripts('/bower_components/jpgjs/jpg.js');
 
 // Import jpeg FFC3 lib
-/* @inline: */ importScripts('/bower_components/jpeg-lossless-decoder-js/release/current/lossless.js');
+/* @inline: */ importScripts('/bower_components/jpeg-lossless-decoder-js/release/current/lossless-min.js');
 
 // Make png.js & config.js worker compatible
 var document = {
@@ -109,6 +55,7 @@ var window = {};
 
 var PNG = window.PNG;
 var KLVReader = WorkerGlobalScope.KLVReader;
+
 
 /** setImageApiUrl(locationUrl, orthancApiUrl)
  *
