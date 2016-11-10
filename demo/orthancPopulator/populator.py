9fc5b8fa04e59afa81bee0caf6b3431a3f36062d
Thibault Piront <gse.nuks@gmail.com>
2016-11-10 15:46:25 +0100

    fix(demo): remove patient protocol from demo

--- demo/orthancPopulator/populator.py
+++ demo/orthancPopulator/populator.py
@@ -35,6 +35,13 @@ def main(orthancUrl):
     orthancSampleDirectory = os.path.abspath(os.path.join(os.path.dirname(__file__), 'orthancSamples'))
     orthanc.uploadFolder(orthancSampleDirectory)
 
+    # Remove patient protocol - series `a51f7c64-b5de5a4b-f934eeb7-6f1d112b-2dbc98e2`
+    # @warning @todo handle this at viewer level
+    # @warning had error 500 on cidmachine w/ docker while fine on localmachine w/ docker (- not found triggers 404)
+    #          cause was due to the docker network not being well removed on error
+    print('removing series a51f7c64-b5de5a4b-f934eeb7-6f1d112b-2dbc98e2 (patient protocol)')
+    orthanc.deleteSeries('a51f7c64-b5de5a4b-f934eeb7-6f1d112b-2dbc98e2') 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Populate an orthanc with the default demo samples")
     parser.add_argument('-u', '--orthanc-url', help="the orthancUrl used for the build", default = None)
