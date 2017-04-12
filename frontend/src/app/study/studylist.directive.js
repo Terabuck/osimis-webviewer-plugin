14b120433fa2722f22a7e399b2b2af15ab6896ed
Thibault Piront <gse.nuks@gmail.com>
2017-04-12 13:00:50 +0200

    refactor(studylist): decide pickable studies in the entrypoint

--- frontend/src/app/study/studylist.directive.js
+++ frontend/src/app/study/studylist.directive.js
@@ -3,78 +3,81 @@
  * @name webviewer.directive:wvStudylist
  *
  * @restrict Element
+ *
+ * @param {Array<string>} wvPickableStudyIds
+ * The list of available study ids.
  */
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('webviewer')
+    angular.module('webviewer')
     .directive('wvStudylist', function ($rootScope, wvConfig) {
-      return {
-        scope: {
-          wvSelectedStudyId: '=?wvSelectedStudyId', // @deprecated
-          wvSelectedStudyIds: '=?wvSelectedStudyIds'
-        },
-        template: [
-          '<button type="button" class="btn btn-default wv-studylist" ',
-          'ng-model="wvSelectedStudyIds" placeholder="Study.." ',
-          'bs-options="study.value as study.label for study in studies" ',
-          'data-multiple="true" bs-select>',
-          '</button>'
-        ].join(''),
-        restrict: 'E',
-        link: function postLink(scope, element, attrs) {
-          scope.studies = [];
-
-          // Default values
-          scope.wvSelectedStudyIds = typeof scope.wvSelectedStudyIds !== 'undefined' ? scope.wvSelectedStudyIds : [];
-
-          // @deprecated keep `wvSelectedStudyId` sync w/ `wvSelectedStudyIds`
-          if (scope.wvSelectedStudyId) {
-            scope.wvSelectedStudyIds[0] = scope.wvSelectedStudyId;
-          }
-          Object.defineProperty(scope, 'wvSelectedStudyId', {
-            get: function() {
-              return scope.wvSelectedStudyIds[0];
+        return {
+            scope: {
+                pickableStudyIds: '=wvPickableStudyIds',
+                wvSelectedStudyId: '=?wvSelectedStudyId', // @deprecated
+                wvSelectedStudyIds: '=?wvSelectedStudyIds'
             },
-            set: function(val) {
-              scope.wvSelectedStudyIds[0] = val;
-            },
-            enumerable: true
-          });
-          // /@deprecated
-          
-          var request = new osimis.HttpRequest();
-          request.setHeaders(wvConfig.httpRequestHeaders);
-          request.setCache(true);
+            template: [
+                '<button type="button" class="btn btn-default wv-studylist" ',
+                'ng-model="wvSelectedStudyIds" placeholder="Study.." ',
+                'bs-options="study.value as study.label for study in studies" ',
+                'data-multiple="true" bs-select>',
+                '</button>'
+            ].join(''),
+            restrict: 'E',
+            link: function postLink(scope, element, attrs) {
+                scope.studies = [];
 
-          request
-          .get(wvConfig.orthancApiURL + '/studies/')
-          .then(function(response) {
-              var studyIds = response.data;
-              scope.studies = studyIds.map(function(studyId) {
-                  return {
-                    label: '?',
-                    value: studyId
-                  };
-              });
-            
-          scope.studies.forEach(function(v) {
-              var studyId = v.value;
+                // Default values
+                scope.pickableStudyIds = typeof scope.pickableStudyIds !== 'undefined' ? scope.pickableStudyIds : [];
+                scope.wvSelectedStudyIds = typeof scope.wvSelectedStudyIds !== 'undefined' ? scope.wvSelectedStudyIds : [];
 
-              var request = new osimis.HttpRequest();
-              request.setHeaders(wvConfig.httpRequestHeaders);
-              request.setCache(true);
+                // @deprecated keep `wvSelectedStudyId` sync w/
+                // `wvSelectedStudyIds`.
+                if (scope.wvSelectedStudyId) {
+                    scope.wvSelectedStudyIds[0] = scope.wvSelectedStudyId;
+                }
+                Object.defineProperty(scope, 'wvSelectedStudyId', {
+                    get: function() {
+                        return scope.wvSelectedStudyIds[0];
+                    },
+                    set: function(val) {
+                        scope.wvSelectedStudyIds[0] = val;
+                    },
+                    enumerable: true
+                });
+                // /@deprecated
 
-              request
-                  .get(wvConfig.orthancApiURL + '/studies/' + studyId)
-                  .then(function(response) {
-                      var study = response.data;
-                      v.label = study.MainDicomTags.StudyDescription;
-                  });
-               });
-          });
-        }
-      };
+                // Update shown studies' information based on pickable study
+                // ids.
+                scope.$watchCollection('pickableStudyIds', function(studyIds) {
+                    scope.studies = studyIds.map(function(studyId) {
+                        return {
+                            label: '?',
+                            value: studyId
+                        };
+                    });
+
+                    // Load study label.
+                    // @todo Move this inside a study model
+                    scope.studies.forEach(function(v) {
+                        var studyId = v.value;
+
+                        var request = new osimis.HttpRequest();
+                        request.setHeaders(wvConfig.httpRequestHeaders);
+                        request.setCache(true);
+
+                        request
+                            .get(wvConfig.orthancApiURL + '/studies/' + studyId)
+                            .then(function(response) {
+                                var study = response.data;
+                                v.label = study.MainDicomTags.StudyDescription;
+                            });
+                    });
+                });
+            }
+        };
     });
 })();
  
