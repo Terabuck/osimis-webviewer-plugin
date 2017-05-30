6bb19273730b8b5f8a6636928368450a3954fed6
Thibault Piront <nuks-home@nuKs-home.local>
2017-05-30 18:43:38 +0200

    chore(tests): enhance testing scripts

--- tests/scripts/startLocally.sh
+++ tests/scripts/startLocally.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 
+# @description
+# This script launch the web viewer tests.
+# 
+# @pre
+# See `scripts/osx/InstallOsXDependencies.sh`. On linux, you must install a
+# simular setup.
+# 
+# @param {boolean} [$1=true]
+# Rebuild test environment.
+
 set -x
 set -e
 
+rebuildTestEnv=${1:-true}
+
+# Start from the repository root.
 previousDir=$(pwd)
 srcRoot="${REPOSITORY_PATH:-$(git rev-parse --show-toplevel)}"
 cd ${srcRoot}
 
-# Start Orthanc Testing
+# Open the test folder.
 cd tests/osimis-test-runner/
-pyvenv env
+
+# Create python environment.
+if [ "$rebuildTestEnv" = true ]; then
+    pyvenv env
+fi
+
+# Load ptython context.
 . env/bin/activate
-pip install -r requirements.txt
+
+# Install python dep.
+if [ "$rebuildTestEnv" = true ]; then
+    pip install -r requirements.txt
+fi
+
+# Launch testes (w/ orthanc environment creation).
 python3 osimis-test-runner.py -p ../../backend/build/
 
+# Go back to previous dir.
 cd ${previousDir}
