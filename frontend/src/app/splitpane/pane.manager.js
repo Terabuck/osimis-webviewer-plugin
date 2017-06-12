446d671540e3d9fb9e8c63675944c44879e4452b
Thibault Piront <gse.nuks@gmail.com>
2017-06-12 16:37:51 +0200

    feat(splitpane): display 1x2 and 2x1 viewport grid layouts

--- frontend/src/app/splitpane/pane.manager.js
+++ frontend/src/app/splitpane/pane.manager.js
@@ -25,8 +25,9 @@
         // Panes.
         // Must keep reference as it's databound in `wvWebviewer` views.
         this.panes = [
-            new osimis.Pane(this._Promise, this._studyManager)
+            new osimis.Pane(this._Promise, this._studyManager, 0, 0)
         ];
+        this.panes[0].isSelected = true;
 
         this.viewedSeriesIds = [];
         this.viewedReportIds = [];
@@ -42,48 +43,96 @@
      * 
      * @name osimis.PaneManager#setLayout
      *
-     * @param {number} x
+     * @param {number} columnCount
      * The number of columns.
      * 
-     * @param {number} y
+     * @param {number} rowCount
      * The number of rows.
      * 
      * @description
      * Change the actual layout of the splitpane.
      */
-    PaneManager.prototype.setLayout = function(x, y) {
-        var actualPaneCount = this.layout.x * this.layout.y;
-        var newPaneCount = x * y;
-        var oldSelectedPane = this.getSelectedPane();
-        var oldSelectedPaneIndex = this.panes.indexOf(oldSelectedPane);
+    PaneManager.prototype.setLayout = function(columnCount, rowCount) {
+        var oldColumnCount = this.layout.x;
+        var oldRowCount = this.layout.y;
+        var i, x, y = null;
 
-        // Clean up old panes.
-        if (newPaneCount < actualPaneCount) {
-            for (var i = newPaneCount; i < actualPaneCount; ++i) {
-                this.panes[i] = undefined; // don't use splice since it changes the indexes from the array/
+        // Update layout count.
+        this.layout.x = columnCount;
+        this.layout.y = rowCount;
+
+        // 1. Remove old panes.`
+        var removedPanes = [];
+        var hasSelectedPaneBeenRemoved = false;
+        for (i=this.panes.length-1; i>=0; --i) {
+            var pane = this.panes[i];
+
+            if (pane.x >= columnCount || pane.y >= rowCount) {
+                this.panes.splice(i, 1);
+
+                // Store removed pane so we can move it (for instance if the
+                // end-user switches columns/rows, so he can keep the panes'
+                // content).
+                removedPanes.push(pane);
+
+                // Make sure we reselect a pane later if none is selected
+                // anymore.
+                if (pane.isSelected) {
+                    hasSelectedPaneBeenRemoved = true;
+                }
             }
         }
 
-        // Configure new panes.
-        else if (newPaneCount > actualPaneCount) {
-            for (var i = actualPaneCount; i < newPaneCount; ++i) {
-                this.panes[i] = new osimis.Pane(this._Promise, this._studyManager);
+        // 3. Create new panes.
+        var columnCountDiff = columnCount - oldColumnCount;
+        var rowCountDiff = rowCount - oldRowCount;
+        if (columnCountDiff > 0 || rowCountDiff > 0) {
+            for (y=0; y<rowCount; ++y) {
+                for (x=0; x<columnCount; ++x) {
+                    // Ignore already created panes.
+                    if (y < oldRowCount && x < oldColumnCount) {
+                        continue;
+                    }
+
+                    // Retrieve panes from the previously removed one.
+                    var pane = removedPanes.pop()
+                    // If none exist, create a new one.
+                    if (!pane) {
+                        pane = new osimis.Pane(this._Promise, this._studyManager, x, y);
+                    }
+                    // Otherwise, move the previously removed pane into its new
+                    // position.
+                    else {
+                        pane.x = x;
+                        pane.y = y;
+                        
+                        // Don't reselect another pane if the current one was
+                        // already selected.
+                        if (pane.isSelected) {
+                            hasSelectedPaneBeenRemoved = false;
+                        }
+                    } 
+
+                    this.panes.push(pane);
+                }
             }
         }
 
-        // Update layout size.
-        this.layout.x = x;
-        this.layout.y = y;
-
-        // Update selected pane (if the currently selected one no longer
-        // exists).
-        if (oldSelectedPaneIndex >= this.layout.x * this.layout.y) {
-            oldSelectedPane.isSelected = false;
-            var newSelectedPane = this.getPane(0);
-            newSelectedPane.isSelected = true;
-
-            // Trigger selected pane changed event.
-            this.onSelectedPaneChanged.trigger(newSelectedPane);
+        // 4. Ensure one pane is selected.
+        if (hasSelectedPaneBeenRemoved) {
+            for (var i=0; i<this.panes.length; ++i) {
+                var pane = this.panes[i];
+                if (!pane.isEmpty()) {
+                    pane.isSelected = true;
+                    hasSelectedPaneBeenRemoved = false;
+                    break;
+                }
+            }
+            // Select an empty pane if no filled pane has been found.
+            if (hasSelectedPaneBeenRemoved && this.panes.length > 0) {
+                var pane = this.panes[0];
+                pane.isSelected = true;
+            }
         }
     };
 
@@ -93,8 +142,11 @@
      * 
      * @name osimis.PaneManager#setPane
      *
-     * @param {number} index
-     * The index of the pane.
+     * @param {number} x
+     * The row of the pane.
+     *
+     * @param {number} y
+     * The column of the pane.
      *
      * @param {object} config
      * See the `osimis.Pane` model for config.
@@ -103,7 +155,7 @@
      * Change the configuration of a pane (its content). Also, consider the
      * content of the pane to have been viewed.
      */
-    PaneManager.prototype.setPane = function(index, config) {
+    PaneManager.prototype.setPane = function(x, y, config) {
         // Assert config.
         if (
             typeof config.seriesId !== 'undefined' &&
@@ -137,15 +189,16 @@
         }
 
         // Update pane's config accordingly.
-        this.panes[index].seriesId = config.seriesId;
-        this.panes[index].reportId = config.reportId;
-        this.panes[index].videoId = config.videoId;
-        this.panes[index].csViewport = config.csViewport;
-        this.panes[index].imageIndex = config.imageIndex;
+        var pane = this.getPane(x, y);
+        pane.seriesId = config.seriesId;
+        pane.reportId = config.reportId;
+        pane.videoId = config.videoId;
+        pane.csViewport = config.csViewport;
+        pane.imageIndex = config.imageIndex;
         // If the new pane is selected, unset the actually selected one.
         if (config.isSelected) {
             var previouslySelectedPane = this.getSelectedPane();
-            var newlySelectedPane = this.panes[index];
+            var newlySelectedPane = pane;
 
             previouslySelectedPane.isSelected = false;
             newlySelectedPane.isSelected = config.isSelected || false;
@@ -161,17 +214,29 @@
      * 
      * @name osimis.PaneManager#getPane
      *
-     * @param {number} index
-     * The index of the pane to retrieve.
+     * @param {number} x
+     * The row of the pane.
+     *
+     * @param {number} y
+     * The column of the pane.
      *
      * @return {osimis.Pane}
      * The pane model.
      *
      * @description
-     * Retrieve the pane model via its index.
+     * Retrieve the pane model via its position.
      */
-    PaneManager.prototype.getPane = function(index) {
-        return this.panes[index];
+    PaneManager.prototype.getPane = function(x, y) {
+        // Search the pane at that position.
+        for (var i=0; i<this.panes.length; ++i) {
+            var pane = this.panes[i];
+            if (pane.isAtPosition(x, y)) {
+                return pane;
+            }
+        }
+
+        // Return null if not found.
+        return null;
     };
 
     /**
@@ -187,15 +252,16 @@
      * Retrieve the currently selected pane model.
      */
     PaneManager.prototype.getSelectedPane = function() {
-        var selectedPaneIndex = 0;
-        this.panes
-            .forEach(function (pane, index) {
-                if (pane && pane.isSelected) {
-                    selectedPaneIndex = index;
-                }
-            });
+        // Return the selected pane.
+        for (var i=0; i<this.panes.length; ++i) {
+            var pane = this.panes[i];
+            if (pane.isSelected) {
+                return pane;
+            }
+        }
 
-        return this.getPane(selectedPaneIndex);
+        // Throw exception if not found.
+        throw new Error('Assert: No selected pane.');
     }
 
     /**
@@ -204,18 +270,21 @@
      * 
      * @name osimis.PaneManager#selectPane
      *
-     * @param {number} index
-     * The index of the pane to select.
+     * @param {number} x
+     * The row of the pane.
+     *
+     * @param {number} y
+     * The column of the pane.
      *
      * @description
      * Toggle a pane selection via its index.
      */
-    PaneManager.prototype.selectPane = function(index) {
+    PaneManager.prototype.selectPane = function(x, y) {
         var previouslySelectedPane = this.getSelectedPane();
-        var newlySelectedPane = this.getPane(index);
+        var newlySelectedPane = this.getPane(x, y);
         
         // Don't do anything if pane is empty.
-        var newlySelectedPane = this.getPane(index);
+        var newlySelectedPane = this.getPane(x, y);
         if (newlySelectedPane.isEmpty()) {
             return;
         };
@@ -236,8 +305,11 @@
      * 
      * @name osimis.PaneManager#isPaneSelected
      *
-     * @param {number} index
-     * The index of the pane to check selection.
+     * @param {number} x
+     * The row of the pane.
+     *
+     * @param {number} y
+     * The column of the pane.
      *
      * @return {boolean}
      * True if the pane is selected.
@@ -245,8 +317,8 @@
      * @description
      * Check if a specific pane is selected.
      */
-    PaneManager.prototype.isPaneSelected = function(index) {
-        var pane = this.getPane(index);
+    PaneManager.prototype.isPaneSelected = function(x, y) {
+        var pane = this.getPane(x, y);
 
         // Return false if the pane doesn't exists. This happens when we switch
         // from 1x2 layout to 2x1 layout. It appears that the switch happens
@@ -277,9 +349,9 @@
      * @description
      * Retrieve a pane index from it's position within the layout.
      */
-    PaneManager.prototype.getPaneIndex = function(x, y) {
-        return (y * this.layout.y) + x;
-    };
+    // PaneManager.prototype.getPaneIndex = function(x, y) {
+    //     return (y * this.layout.x) + x;
+    // };
 
     /**
      * @ngdoc method
