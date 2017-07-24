dcde37440585f78e61b9871d99cbe58371df9536
Thibault Piront nuKs <gse.nuks@gmail.com>
2017-07-24 11:27:15 +0200

    feat(viewport): display orientation markers

--- frontend/src/app/image/instance.manager.js
+++ frontend/src/app/image/instance.manager.js
@@ -37,16 +37,42 @@
              * @methodOf webviewer.service:wvInstanceManager
              *
              * @name osimis.InstanceManager#setTags
-             * @param {string} id The id of the instance (orthanc format)
-             * @param {object} tags Object containing tags on format {tag1: content1, ...}
              * 
+             * @param {string} id 
+             * The id of the instance (orthanc format).
+             *
+             * @param {object} tags
+             * Object containing tags on format {tag1: content1, ...}
+             *
              * @description
              * Set the tags of an instance.
              * 
              * Used mainly for optimization: retrieving all simplified tags at one single request within the wvSeriesManager
              * instead of many requests for each instances.
              */
-            setTags: setTags
+            setTags: setTags,
+            /**
+             * @ngdoc method
+             * @methodOf webviewer.service:wvInstanceManager
+             *
+             * @name osimis.InstanceManager#onTagsSet
+             * 
+             * @param {function} callback
+             * A callback function triggered each time a tag as been defined.
+             * 
+             * - @param {string} id
+             * The id of the instance (orthanc format).
+             *
+             * - @param {object} tags
+             * Object containing tags on format {tag1: content1, ...}
+             * 
+             * @description
+             * A callback function triggered each time a tag as been defined.
+             * 
+             * Required as some part of the cornerstone API doesn't fit well
+             * with the getter/setter we use (because they use promises).
+             */
+            onTagsSet: new osimis.Listener()
         };
 
         /**
@@ -68,7 +94,7 @@
         ////////////////
 
         function getTags(id) {
-        	// Load image tags if not already in loading
+        	// Load image tags if not already in loading.
         	if (!_tagsByInstances.hasOwnProperty(id)) {
                 var request = new osimis.HttpRequest();
                 request.setHeaders(wvConfig.httpRequestHeaders);
@@ -79,22 +105,25 @@
 
                         return tags;
                     }, function(err) {
-                    	// @todo uncache & do something with the error
+                    	// @todo uncache & do something with the error.
                     	
                     	return $q.reject(err);
                     });
             }
 
-            // Return the tags
+            // Return the tags.
         	return _tagsByInstances[id];
         }
 
         function setTags(id, tags) {
-        	// Always wrap tags in a promise to stay consistant with the API
-        	tags = $q.when(tags);
+        	// Always wrap tags in a promise to stay consistant with the API.
+        	var tagsPromise = $q.when(tags);
         	
-        	// Store the tags
-            _tagsByInstances[id] = tags;
+        	// Store the tags.
+            _tagsByInstances[id] = tagsPromise;
+
+            // Trigger event.
+            service.onTagsSet.trigger(id, tags);
         }
     }
 })();
