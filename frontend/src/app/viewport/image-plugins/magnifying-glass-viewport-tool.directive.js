a52c862b8f8eb649c59eac1bd32e63df0578adb3
Thibault Piront nuKs <gse.nuks@gmail.com>
2017-07-28 11:34:43 +0200

    sync

--- frontend/src/app/viewport/image-plugins/magnifying-glass-viewport-tool.directive.js
+++ frontend/src/app/viewport/image-plugins/magnifying-glass-viewport-tool.directive.js
@@ -48,22 +48,19 @@
                 // Activate back the magnifying.
                 if (newConfig.enabled) {
                     tool.activate();
-                    (function() {
-                        // The `cornerstoneTools.magnify.setConfiguration` method
-                        // doesn't update the glass size. We have to manually change
-                        // the magnifying glass size.
-                        if (oldConfig.magnifyingGlassSize !== newConfig.magnifyingGlassSize) {
-                            magnifyingGlassCanvasEl.width = csConfig.magnifySize;
-                            magnifyingGlassCanvasEl.height = csConfig.magnifySize;
-                        }
-                    });
+                    // Ensure the magnifying glass always stay on top of
+                    // everything.
+                    var magnifyingGlassCanvasJqEl = $('.magnifyTool');
+                    var magnifyingGlassCanvasEl = magnifyingGlassCanvasJqEl[0];
+                    magnifyingGlassCanvasJqEl.css('z-index', 1000000);
+                    // The `cornerstoneTools.magnify.setConfiguration` method
+                    // doesn't update the glass size. We have to manually change
+                    // the magnifying glass size.
+                    if (oldConfig.magnifyingGlassSize !== newConfig.magnifyingGlassSize) {
+                        magnifyingGlassCanvasEl.width = csConfig.magnifySize;
+                        magnifyingGlassCanvasEl.height = csConfig.magnifySize;
+                    }
                 }
-
-                // Ensure the magnifying glass always stay on top of
-                // everything.
-                var magnifyingGlassCanvasJqEl = $('.magnifyTool');
-                var magnifyingGlassCanvasEl = magnifyingGlassCanvasJqEl[0];
-                magnifyingGlassCanvasJqEl.css('z-index', 1000000);
             }, true);
         }
 
@@ -71,6 +68,11 @@
         function MagnifyingGlassCtrl() {
             WvBaseTool.call(this, 'magnify', 'magnifyTouchDrag');
 
+            // BaseTool class as been made for annotation. This is not one.
+            // We overide this method so the glass is not shown once toggled
+            // off. When we deactivate an annotation, we let the annotation
+            // shown, but only deactivate the inputs.
+            // For tools related to cornerstone (@todo split BaseTool in AnnotationTools & others)
             this._deactivateInputs = function(viewport) {
                 // Unlisten to events
                 var enabledElement = viewport.getEnabledElement();
@@ -83,7 +85,41 @@
                     cornerstoneTools[this.toolName2].disable(enabledElement);
                 }
             };
+            this.register = function(viewport) {
+                this.viewports.push(viewport)
 
+                if (this.isActivated) {
+                    this.activate(viewport);
+                }
+            };
+            this.unregister = function(viewport) {
+                if (cornerstoneTools[this.toolName]) {
+                    // Set tool in disable mode (it's a 1D state machine with 4
+                    // states) - don't display annotations & ignore inputs.
+                    // 1. Retrieve DOM element
+                    var enabledElement = viewport.getEnabledElement();
+                    // 2. Ignore exception if no image is shown in the viewport
+                    var isElementEnabled = undefined;
+                    try {
+                        isElementEnabled = true;
+                        cornerstone.getEnabledElement(enabledElement); 
+                    }
+                    catch (exc) {
+                        isElementEnabled = false;
+                    }
+                    // 3. Change tool state
+                    // if (isElementEnabled) {
+                    //     cornerstoneTools[this.toolName].enable(enabledElement, 1);
+                    //     if (this.toolName2) {
+                    //         cornerstoneTools[this.toolName2].activate(enabledElement);
+                    //     }
+                    // }
+                }
+
+                this._unlistenModelChange(viewport);
+                
+                _.pull(this.viewports, viewport);
+            };
         }
         MagnifyingGlassCtrl.prototype = Object.create(WvBaseTool.prototype)
         MagnifyingGlassCtrl.prototype.constructor = MagnifyingGlassCtrl;
