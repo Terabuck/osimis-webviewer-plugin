d54f8e42e6af627de79831ef1823a38f67f6fa66
Thibault Piront nuKs <gse.nuks@gmail.com>
2017-07-28 17:53:22 +0200

    fix(scripts): fix dev script

--- frontend/scripts/buildJsLocally.sh
+++ frontend/scripts/buildJsLocally.sh
@@ -14,6 +14,9 @@ npm install --unsafe-perm --python=python2.7
 # install 2 times (for random phantom_js download issues)
 npm install --unsafe-perm --python=python2.7
 
+# Restore dependency developed by Osimis within the webviewer
+git checkout node_modules/gulp-injectInlineWorker/index.js
+
 bower install --allow-root
 
 #first run the unit tests
