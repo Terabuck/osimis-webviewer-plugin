1b49f64d054ec245e934c8b9b2afcba915e4d03a
Alain Mazy <am@osimis.io>
2017-08-23 21:45:18 +0200

    cleanup

--- demo/scripts/buildDocker.sh
+++ demo/scripts/buildDocker.sh
@@ -16,6 +16,6 @@ wvbContainerId=$(docker create osimis/orthanc-webviewer-plugin:latest-local)
 docker cp --follow-link "$wvbContainerId:/usr/share/orthanc/plugins/libOsimisWebViewer.so" binaries/
 docker rm $wvbContainerId
 
-# Build demo docker image with latest Orthanc, latest sample data but not yet the wvb .so
+# Build demo docker image
 docker rmi -f osimis/orthanc-webviewer-plugin/demo:latest-local || true # @todo Use trap to clean image instead
 docker build -t osimis/orthanc-webviewer-plugin/demo:latest-local .
