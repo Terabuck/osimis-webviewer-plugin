2b6e3a3bdfb58903587cf715e7d099ce34cdaa08
Jérémy Evrard <je@osimis.io>
2017-11-23 16:37:17 +0100

    front-end: add oneCol displayMode

--- frontend/src/app/layout/layout-main.directive.js
+++ frontend/src/app/layout/layout-main.directive.js
@@ -27,7 +27,8 @@
             vm.isTopPaddingEnabled = wvLayout.isTopEnabled;
             vm.isRightPaddingEnabled = wvLayout.isRightEnabled && !wvLayout.isRightClosed;
             vm.isBottomPaddingEnabled = wvLayout.isBottomEnabled;
-            vm.isLeftPaddingEnabled = wvLayout.isLeftEnabled && !wvLayout.isLeftClosed;
+            vm.isLeftPaddingEnabled = wvLayout.isLeftEnabled && !wvLayout.isLeftSmall && !wvLayout.isLeftClosed;
+            vm.isSmallLeftPaddingEnabled = wvLayout.isLeftEnabled && wvLayout.isLeftSmall && !wvLayout.isLeftClosed;
 
             scope.$watch(function() {
                 return [
@@ -37,12 +38,14 @@
                     wvLayout.isBottomEnabled,
                     wvLayout.isLeftEnabled,
                     wvLayout.isLeftClosed,
+                    wvLayout.isLeftSmall
                 ];
             }, function(newValues) {
                 vm.isTopPaddingEnabled = newValues[0];
                 vm.isRightPaddingEnabled = newValues[1] && !newValues[2];
                 vm.isBottomPaddingEnabled = newValues[3];
-                vm.isLeftPaddingEnabled = newValues[4] && !newValues[5];
+                vm.isLeftPaddingEnabled = newValues[4] && !newValues[6] && !newValues[5];
+                vm.isSmallLeftPaddingEnabled = newValues[4] && newValues[6] && !newValues[5];
             }, true);
         }
     }
