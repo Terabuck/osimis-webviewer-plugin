f6f47d6cecfeaf665b41c727c429dfac9b79c5f4
amazy <am@osimis.io>
2018-11-13 11:51:25 +0100

    fix error wrt tooltip on touch devices

--- frontend/src/app/webviewer.module.js
+++ frontend/src/app/webviewer.module.js
@@ -43,12 +43,9 @@
             // or set some global variable
             window.USER_IS_TOUCHING = true;
 
-            $tooltipProvider.options({trigger: "dontTrigger"});
+            $tooltipProvider.defaults.trigger = "dontTrigger";
             console.log('disabling tooltip', $tooltipProvider);
 
-            // or set your app's state however you normally would
-            myFrameworkOfChoice.dispatchEvent('USER_IS_TOUCHING', true);
-        
             // we only need to know once that a human touched the screen, so we can stop listening now
             window.removeEventListener('touchstart', onFirstTouch, false);
         }, false);
